829d43eabf852a30ec3656eac8487bf7156f5fdd
Ray Luxembourg <rayluxembourg@gmail.com>
2019-04-29 18:13:27 +0300

    deploymnet change

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,6 +1,4 @@
 import { GraphQLObjectType } from 'graphql';
-import * as functions from "firebase-functions";
 export declare const ElementType: GraphQLObjectType<Element, any, {
     [key: string]: any;
 }>;
-export declare const service: functions.HttpsFunction;
--- build/index.js
+++ build/index.js
@@ -13,7 +13,6 @@ const jsdom_1 = require("jsdom");
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const url_1 = require("url");
-const functions = require("firebase-functions");
 function sharedFields() {
     const selector = {
         type: graphql_1.GraphQLString,
@@ -241,9 +240,8 @@ app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true,
 }));
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
-exports.service = functions.https.onRequest(app);
 // Make this importable with ES6
 // schema['default'] = schema
 // export default schema
